30e49005278ca06837e61747dfd9b9a4125baf2d
Guillaume BENARD <guillaume@benard.io>
2021-12-05 14:06:50 +0100

    chore: setup web-app
    * create-react-app
    * eslint + prettier

---
+++ apps/web-app/src/index.tsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
